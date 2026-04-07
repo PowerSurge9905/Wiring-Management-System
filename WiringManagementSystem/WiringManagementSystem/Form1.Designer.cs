39086cf1bb8405755661cbfdb88d210fe68c2df3
stephsq <stephsquach@gmail.com>
2026-04-07 19:26:14 -0400

    Added the button logic for treeview nodes that work with textbox until furthur progress can be made on how the data connected with DB

--- WiringManagementSystem/WiringManagementSystem/Form1.Designer.cs
+++ WiringManagementSystem/WiringManagementSystem/Form1.Designer.cs
@@ -28,27 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            tre_WiringManagement = new TreeView();
+            tree_WiringManagement = new TreeView();
             sqlDataAdapter1 = new Microsoft.Data.SqlClient.SqlDataAdapter();
             listBox1 = new ListBox();
             btnAddDevice = new Button();
             btnEditDevice = new Button();
             btnEditConnection = new Button();
             btnDeleteDevice = new Button();
+            txtBox = new TextBox();
             SuspendLayout();
             // 
-            // tre_WiringManagement
+            // tree_WiringManagement
             // 
-            tre_WiringManagement.Location = new Point(12, 12);
-            tre_WiringManagement.Name = "tre_WiringManagement";
-            tre_WiringManagement.Size = new Size(513, 221);
-            tre_WiringManagement.TabIndex = 0;
+            tree_WiringManagement.Location = new Point(12, 12);
+            tree_WiringManagement.Name = "tree_WiringManagement";
+            tree_WiringManagement.Size = new Size(513, 198);
+            tree_WiringManagement.TabIndex = 0;
             // 
             // listBox1
             // 
             listBox1.FormattingEnabled = true;
             listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(12, 239);
+            listBox1.Location = new Point(12, 216);
             listBox1.Name = "listBox1";
             listBox1.Size = new Size(513, 94);
             listBox1.TabIndex = 1;
@@ -61,6 +62,7 @@
             btnAddDevice.TabIndex = 2;
             btnAddDevice.Text = "Add Device";
             btnAddDevice.UseVisualStyleBackColor = true;
+            btnAddDevice.Click += btnAddDevice_Click;
             // 
             // btnEditDevice
             // 
@@ -70,6 +72,7 @@
             btnEditDevice.TabIndex = 3;
             btnEditDevice.Text = "Edit Device";
             btnEditDevice.UseVisualStyleBackColor = true;
+            btnEditDevice.Click += btnEditDevice_Click;
             // 
             // btnEditConnection
             // 
@@ -79,6 +82,7 @@
             btnEditConnection.TabIndex = 4;
             btnEditConnection.Text = "Edit Connections";
             btnEditConnection.UseVisualStyleBackColor = true;
+            btnEditConnection.Click += btnEditConnection_Click;
             // 
             // btnDeleteDevice
             // 
@@ -88,31 +92,42 @@
             btnDeleteDevice.TabIndex = 5;
             btnDeleteDevice.Text = "Delete Device";
             btnDeleteDevice.UseVisualStyleBackColor = true;
+            btnDeleteDevice.Click += btnDeleteDevice_Click;
+            // 
+            // txtBox
+            // 
+            txtBox.Location = new Point(12, 316);
+            txtBox.Name = "txtBox";
+            txtBox.Size = new Size(245, 23);
+            txtBox.TabIndex = 6;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(537, 402);
+            Controls.Add(txtBox);
             Controls.Add(btnDeleteDevice);
             Controls.Add(btnEditConnection);
             Controls.Add(btnEditDevice);
             Controls.Add(btnAddDevice);
             Controls.Add(listBox1);
-            Controls.Add(tre_WiringManagement);
+            Controls.Add(tree_WiringManagement);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private TreeView tre_WiringManagement;
+        private TreeView tree_WiringManagement;
         private Microsoft.Data.SqlClient.SqlDataAdapter sqlDataAdapter1;
         private ListBox listBox1;
         private Button btnAddDevice;
         private Button btnEditDevice;
         private Button btnEditConnection;
         private Button btnDeleteDevice;
+        private TextBox txtBox;
     }
 }
